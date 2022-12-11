dc57bce5d8e06d16f96bcf6a4c07d09d53a21bf1
Haikal Rowi <haikalbintang27@gmail.com>
2022-12-11 11:57:08 +0700

    Revert "fix"
    This reverts commit 80abf81b59ba8473405313254db554d7307443c6.

--- databases/dbPerpusOpen.ts
+++ databases/dbPerpusOpen.ts
@@ -3,7 +3,7 @@ import sqlite3 from "sqlite3";
 
 export default async function () {
   return open({
-    filename: './pages/database/dbPerpus',
+    filename: './databases/dbPerpus',
     driver: sqlite3.Database
   })
 }
--- pages/api/book/isbn/[isbn].ts
+++ pages/api/book/isbn/[isbn].ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import dbPerpusOpen from "../../../database/dbPerpusOpen";
+import dbPerpusOpen from "../../../../databases/dbPerpusOpen";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
