80abf81b59ba8473405313254db554d7307443c6
Haikal Rowi <haikalbintang27@gmail.com>
2022-12-11 09:44:05 +0700

    fix

--- pages/database/dbPerpusOpen.ts
+++ pages/database/dbPerpusOpen.ts
@@ -3,7 +3,7 @@ import sqlite3 from "sqlite3";
 
 export default async function () {
   return open({
-    filename: './databases/dbPerpus',
+    filename: './pages/database/dbPerpus',
     driver: sqlite3.Database
   })
 }
